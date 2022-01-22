f51f33cbe479ca6e94cda1373be835af1c3831b7
liron <liron7722@gmail.com>
2022-01-22 16:43:02 +0200

    fixing the dummy test

--- tests/component_up.test.js
+++ tests/component_up.test.js
@@ -4,8 +4,8 @@ const request = supertest(app)
 
 describe("/test endpoint", () => {
     it("should return a response", async () => {
-        const response = await request.get("/users")
+        const response = await request.get("/users/api/test/all")
         expect(response.status).toBe(200)
-        expect(response.text).toBe("respond with a resource");
+        expect(response.text).toBe("Public Content.");
     })
 })
