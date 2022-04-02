7d68cc01e7241ce8bf4392cfe6bd5e448a9c956e
liron <liron7722@gmail.com>
2022-04-03 00:04:08 +0300

    lint

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,68 +1,20 @@
-var express = require('express');
-const logger = require('../models/logger')
-var router = express.Router();
+const express = require('express');
+const { authJwt } = require('../subscribers');
+const controller = require('../services/user.controller');
 
-const childLogger = logger.child({ service: 'user pages' });
+const router = express.Router();
 
-// /* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   res.send('respond with a resource');
-//   childLogger.trace('Someone load user page')
-// });
-
-// module.exports = router;
-
-const { authJwt } = require("../subscribers");
-const controller = require("../services/user.controller");
-
-module.exports = function(app) {
-  app.use(function(req, res, next) {
-    res.header(
-      "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept"
-    );
-    next();
-  });
-
-  app.get("/api/test/all", controller.allAccess);
-
-  app.get("/api/test/user", [authJwt.verifyToken], controller.userBoard);
-
-  app.get(
-    "/api/test/mod",
-    [authJwt.verifyToken, authJwt.isModerator],
-    controller.moderatorBoard
+router.use((req, res, next) => {
+  res.header(
+    'Access-Control-Allow-Headers',
+    'x-access-token, Origin, Content-Type, Accept',
   );
+  next();
+});
 
-  app.get(
-    "/api/test/admin",
-    [authJwt.verifyToken, authJwt.isAdmin],
-    controller.adminBoard
-  );
-};
+router.get('/api/test/all', controller.allAccess);
+router.get('/api/test/user', [authJwt.verifyToken], controller.userBoard);
+router.get('/api/test/mod', [authJwt.verifyToken, authJwt.isModerator], controller.moderatorBoard);
+router.get('/api/test/admin', [authJwt.verifyToken, authJwt.isAdmin], controller.adminBoard);
 
-module.exports = function(app) {
-  app.use(function(req, res, next) {
-    res.header(
-      "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept"
-    );
-    next();
-  });
-
-  app.get("/api/test/all", controller.allAccess);
-
-  app.get("/api/test/user", [authJwt.verifyToken], controller.userBoard);
-
-  app.get(
-    "/api/test/mod",
-    [authJwt.verifyToken, authJwt.isModerator],
-    controller.moderatorBoard
-  );
-
-  app.get(
-    "/api/test/admin",
-    [authJwt.verifyToken, authJwt.isAdmin],
-    controller.adminBoard
-  );
-};
+module.exports = router;
