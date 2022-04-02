7ca5b7acae22ee69911aed4923466640800486e6
liron <liron7722@gmail.com>
2022-04-03 00:18:48 +0300

    validators initial commit

---
+++ scripts/validators.js
@@ -0,0 +1,11 @@
+exports.validateSignIn = (req) => {
+  // validate request
+  if (req) return true;
+  return false;
+};
+
+exports.validateSignUp = (req) => {
+  // validate request
+  if (req) return true;
+  return false;
+};
