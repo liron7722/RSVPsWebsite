a677ff419f7719d7f377db612cea5f2ff68c7d33
liron <liron7722@gmail.com>
2022-04-03 00:35:49 +0300

    generate secret if enviorment variable is empty

--- config/auth.config.js
+++ config/auth.config.js
@@ -1,3 +1,13 @@
+function generateSecret(length) {
+  let result = '';
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let charactersLength = characters.length;
+  for ( let i = 0; i < length; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
+
 module.exports = {
-  secret: process.env.secret,
+  secret: process.env.secret || generateSecret(40),
 };
