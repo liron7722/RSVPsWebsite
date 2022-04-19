d4db226cbaa3526ab7e585d53add04c16f46d86a
liron <liron7722@gmail.com>
2022-04-19 17:48:00 +0300

    lint changes

--- config/auth.config.js
+++ config/auth.config.js
@@ -1,12 +1,12 @@
 function generateSecret(length) {
   let result = '';
-  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let charactersLength = characters.length;
-  for ( let i = 0; i < length; i++ ) {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i += 1) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
   return result;
-};
+}
 
 module.exports = {
   secret: process.env.secret || generateSecret(40),
